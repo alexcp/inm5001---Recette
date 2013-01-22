ac4672b2f58587eaa50df5c6171fefca00f71158
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-01-22 14:21:05 -0500

    Default template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sign_in_with provider
+    link_to "Sign In With your #{provider.titleize} account", "/auth/#{provider}", :class => "btn"
+  end
+
+  def sign_in_buttons
+    content_tag :div do
+      %w{facebook twitter openid}.map {|provider| concat content_tag :p ,sign_in_with(provider) }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Recettes::Application.routes.draw do
+  root :to => "home#index"
+
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
