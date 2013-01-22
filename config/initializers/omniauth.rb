34e0ca79781804ef67513f129227152fd15cf8d4
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-01-21 21:37:47 -0500

    add facebook app id & secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem' 
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  # IMPORTANT: Tout les informations ici doivent être secretes
+  provider :facebook, "273444086117075", "1d9fd55d0a190557c006971b5c26a128"
   provider :twitter, "CONSUMER_KEY", "CONSUMER_SECRET"
   provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
 end
