d6340cbaa41d42632a82c08188b83a5c0215e3c7
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-01-24 11:19:42 -0500

    twitter login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem' 
   # IMPORTANT: Tout les informations ici doivent être secretes
   provider :facebook, "273444086117075", "1d9fd55d0a190557c006971b5c26a128"
-  provider :twitter, "CONSUMER_KEY", "CONSUMER_SECRET"
+  provider :twitter, "tQkoVVNGEidkTIAuqkUg", "sRqCilCoYQ35NBimaH9K3n83q5g9UoJqK6t470YBw"
   provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
 end
