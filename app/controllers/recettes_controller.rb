8650c7c2303f7823c3003a47ce6ccf5fd76d0066
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-02-05 21:58:07 -0500

    ajouter message d'erreur si un utilisateur nest pas connecter et creer une recette

--- app/controllers/recettes_controller.rb
+++ app/controllers/recettes_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class RecettesController < ApplicationController
   # GET /recettes
   # GET /recettes.json
@@ -24,7 +25,12 @@ class RecettesController < ApplicationController
   # GET /recettes/new
   # GET /recettes/new.json
   def new
-    @recette = current_user.recettes.new
+    begin
+      @recette = current_user.recettes.new
+    rescue NoMethodError
+      redirect_to root_path, :flash => {:error => "Vous devez être enregistré."}
+      return
+    end
 
     respond_to do |format|
       format.html # new.html.erb
@@ -44,7 +50,7 @@ class RecettesController < ApplicationController
 
     respond_to do |format|
       if @recette.save
-        format.html { redirect_to @recette, notice: 'Recette was successfully created.' }
+        format.html { redirect_to @recette, :flash => {:notice => "La recette à été enregistré."} }
         format.json { render json: @recette, status: :created, location: @recette }
       else
         format.html { render action: "new" }
@@ -60,7 +66,7 @@ class RecettesController < ApplicationController
 
     respond_to do |format|
       if @recette.update_attributes(params[:recette])
-        format.html { redirect_to @recette, notice: 'Recette was successfully updated.' }
+        format.html { redirect_to @recette, :flash => {:notice => "La recette à été enregistré."} }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
