c85cc46f72ba7daa68bdfbad3598f70ab834b549
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-02-20 19:48:40 -0500

    ajout de validation

--- app/models/ingredient_recette.rb
+++ app/models/ingredient_recette.rb
@@ -1,14 +1,19 @@
 class IngredientRecette < ActiveRecord::Base
   attr_accessible :nom, :quantite, :unite_de_mesure
   attr_accessor :nom, :quantite, :unite_de_mesure
+  validates_presence_of :nom, :quantite, :unite_de_mesure, :ingredient
 
   belongs_to :ingredient
   belongs_to :recette
   
-  before_save :assigne_un_ingredient
+  before_validation :assigne_un_ingredient
 
   def assigne_un_ingredient
-    self.ingredient = trouve_lingredient_correspondante
+    if
+      self.ingredient = trouve_lingredient_correspondante
+    else
+      errors.add :nom, "Recette Introuvable"
+    end
   end
 
   def trouve_lingredient_correspondante
