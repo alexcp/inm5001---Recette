ad4d48cdf75cfed92f32852cce2fd7a06c6a6b86
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-02-24 23:18:52 -0500

    ajout de validations aux ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 class Ingredient < ActiveRecord::Base
   attr_accessible :nom, :unite_de_mesure,:portion,:prix,:proteine,:glucide,:gras
-
   has_many :ingredient_recettes
-  validates_presence_of :nom, :unite_de_mesure
+
+  validates_presence_of :unite_de_mesure
+  validates :nom, :presence => true, :uniqueness => { :case_sensitive => false }
 
   UNITE_DE_MESURE = ["gramme","mililitre","litre","cuillière à table","cuillière a thé","tasse"]
 
