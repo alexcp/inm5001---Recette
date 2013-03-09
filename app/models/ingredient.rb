9dee29941482cc3c027ef1c9859fbcde3140e381
Raph <raph@raph.(none)>
2013-03-09 15:57:59 -0500

    Corrections du au changement de table IngredientRecette

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,18 +1,9 @@
 # encoding: utf-8
 class Ingredient < ActiveRecord::Base
-  attr_accessible :nom, :unite_de_mesure,:portion,:prix,:proteine,:glucide,:gras
+  attr_accessible :nom ,:prix,:proteine,:glucide,:gras
   has_many :ingredient_recettes
 
-  validates_presence_of :unite_de_mesure
   validates :nom, :presence => true, :uniqueness => { :case_sensitive => false }
 
-  UNITE_DE_MESURE = ["gramme(s)","mililitre(s)","litre(s)","cuillière(s) à table","cuillière(s) a thé","tasse(s)", "unité(s)", "douzaine(s)"]
 
-  def obtenir_unite_de_mesure
-    UNITE_DE_MESURE[unite_de_mesure]
-  end
-
-  def valeur_de_unite unite_en_mot
-    UNITE_DE_MESURE.index unite_en_mot
-  end
 end
--- app/models/ingredient_recette.rb
+++ app/models/ingredient_recette.rb
@@ -1,18 +1,28 @@
 class IngredientRecette < ActiveRecord::Base
-  attr_accessible :nom, :quantite, :unite_de_mesure
-  attr_accessor :nom, :quantite, :unite_de_mesure
-  validates_presence_of :nom, :quantite, :unite_de_mesure, :ingredient
+  attr_accessible :nom, :portion, :unite_de_mesure
+  attr_accessor :nom, :portion, :unite_de_mesure
+  validates_presence_of :nom, :portion, :unite_de_mesure, :ingredient
 
   belongs_to :ingredient
   belongs_to :recette
   
   before_validation :assigne_un_ingredient
 
+  UNITE_DE_MESURE = ["gramme(s)","mililitre(s)","litre(s)","cuilliere(s) a table","cuilliere(s) a the","tasse(s)", "unite(s)", "douzaine(s)"]
+
+  def obtenir_unite_de_mesure
+    UNITE_DE_MESURE[unite_de_mesure]
+  end
+
+  def valeur_de_unite unite_en_mot
+    UNITE_DE_MESURE.index unite_en_mot
+  end
+
   def assigne_un_ingredient
     if
       self.ingredient = trouve_lingredient_correspondante
     else
-      errors.add :nom, "Recette Introuvable"
+      errors.add :nom, "Ingredient non existant introuvable"
     end
   end
 
