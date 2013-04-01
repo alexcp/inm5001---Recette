7d1be5fd4368f0b938f48d2f291c95fb37bc74cf
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-04-01 18:20:29 -0400

    removed some useless stuff

--- app/assets/javascripts/recettes.js
+++ app/assets/javascripts/recettes.js
@@ -15,25 +15,4 @@ $(document).ready(function(){
 			reader.readAsDataURL(input.files[0]);
 		}
 	}
-
-    
-
-	$(document).find("[readonly]").each(function(value) {
-
-		$(this).prop('disabled', true);
-		$(this).css({'background-color' : 'transparent', 'border-color': 'transparent' , 'outline': 'none' , 'box-shadow':'none'  })
-	})
-
-	$("#modal_button").click(function(){
-
-  // $("textarea").css({'background-color' : 'transparent', 'border': 'none' , 'outline': 'none' , 'box-shadow':'none'  })
-  $(document).find("[readonly]").each(function(value) {
-  	$(document).find(".cache").css("visibility","visible")
-  	$(this).attr('readonly',false);
-  	$(this).prop('disabled', false);
-  	$(this).css({'background-color' : '' ,  'border': '' , 'outline': '' , 'box-shadow':'' })
-//$(this).removeClass().addClass("invisibleInput");
-})
-
-});
 });
