301b3506dedc67e5b0b2182a119c885ce4ad40da
Raph <raph@raph.(none)>
2013-04-07 15:00:19 -0400

    Ingredient autocomplete

--- app/assets/javascripts/recettes.js
+++ app/assets/javascripts/recettes.js
@@ -2,9 +2,12 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 
+	
+
 	$('.upload').change(function() {
 		readURL(this);
 	});
+
 
 	function readURL(input) {
 		if (input.files && input.files[0]) {
@@ -15,4 +18,30 @@ $(document).ready(function(){
 			reader.readAsDataURL(input.files[0]);
 		}
 	}
+
 });
+ 	function recherchable(fieldTorecherchable) {  
+
+   		$(fieldTorecherchable).typeahead({
+      source: recherche()
+    	});
+   	}
+
+
+  	function recherche() {  
+  		var  liste_nom
+
+  	    $.ajax({
+		    type: 'GET',
+		    url: "/recherche_ingredient",
+		    data: 'param1=""',
+		    dataType: "json",
+		    async: false,
+		    success: function(data){liste_nom= data;}
+		})
+
+		return   $.map( liste_nom, function(n){
+				  return n["nom"];
+				});	
+
+  	};
