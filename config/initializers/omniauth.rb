4dccaf3a69518fbee83c13ece1094a79d32616b3
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-05-23 10:59:33 -0300

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem' 
-  # IMPORTANT: Tout les informations ici doivent être secretes
-  provider :facebook, "273444086117075", "1d9fd55d0a190557c006971b5c26a128"
-  provider :twitter, "tQkoVVNGEidkTIAuqkUg", "sRqCilCoYQ35NBimaH9K3n83q5g9UoJqK6t470YBw"
-  provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+  # These API Key are now deactivated.
+  # If you want to use the app. Get new keys and store them in Environement Variables.
+  #provider :facebook, "273444086117075", "1d9fd55d0a190557c006971b5c26a128"
+  #provider :twitter, "tQkoVVNGEidkTIAuqkUg", "sRqCilCoYQ35NBimaH9K3n83q5g9UoJqK6t470YBw"
 end
